c85cf68935c778ee2c73dfbde78a1d32f451e4e0
Shadowwws <rcarre03@gmail.com>
2020-05-06 17:46:18 +0200

    Main fix

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,15 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.TilePane;
-import javafx.stage.Stage;
+import model.Battlefield;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			TilePane root = FXMLLoader.load(getClass().getResource("vueTest.fxml"));
-			Scene scene = new Scene(root,800,600);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+public class Main {
+    public static void main(String[] args){
+        Battlefield b1 = new Battlefield("battlefields/battlefield1.json");
+        int[][] battlefield = b1.getBattlefield();
+        for (int j = 0; j < battlefield[0].length; j++) {
+            for (int i = 0; i < battlefield.length; i++) {
+                System.out.print(battlefield[i][j]+"   ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
