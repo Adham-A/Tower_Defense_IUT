5c7aa22e32b91b1ca3c124277fb671d35a550c30
adhamawad06@gmail.com <adhamawad06@gmail.com>
2020-05-06 14:59:20 +0200

    Added JSON parse method for battlefield class

--- src/model/Battlefield.java
+++ src/model/Battlefield.java
@@ -1,5 +1,44 @@
 package model;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Battlefield {
-	//TO DO
+	
+	private int width;
+	private int height;
+	private int[][] battlefield;
+	
+	public Battlefield(String path) throws FileNotFoundException, IOException, ParseException {
+		parseTerrainFromFile(path);
+	}
+	
+	public void parseTerrainFromFile(String path) throws FileNotFoundException, IOException, ParseException {
+		Object obj = new JSONParser().parse(new FileReader(path)); 
+		JSONObject jo = (JSONObject) obj;
+		this.height = ((Long) jo.get("height")).intValue();
+		this.width = ((Long) jo.get("width")).intValue();
+		
+		int[][] battlefield = new int [this.height][this.width];
+
+		JSONArray jsonArray = (JSONArray) jo.get("data");
+		Iterator<?> iterator = jsonArray.iterator();
+
+		for (int line = 0; line < this.width; line++) {
+			for (int column = 0; column < this.height; column++) {
+				if(iterator.hasNext()) {
+					battlefield[column][line] = ( (Long) iterator.next()).intValue();
+				}
+			}
+		}
+		this.battlefield = battlefield;
+	}
+
 }
