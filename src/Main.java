82518aa1a2dddb9c7f924d4ddf72d63d25ccde3c
Shadowwws <rcarre03@gmail.com>
2020-05-10 16:42:17 +0200

    Test show view

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import model.Battlefield;
 
-public class Main {
+/*public class Main {
     public static void main(String[] args){
         Battlefield b1 = new Battlefield("battlefields/battlefield1.json");
         int[][] battlefield = b1.getBattlefield();
@@ -12,4 +12,30 @@ public class Main {
         }
         System.out.println();
     }
+}*/
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			TilePane root = FXMLLoader.load(getClass().getResource("view/view.fxml"));
+			Scene scene = new Scene(root,240,432);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,59 @@
 package controller;
 
-public class Controller {
-	//TO DO
-	//TO DO 2
-    //TO DO 3
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import model.Battlefield;
+
+public class Controller implements Initializable{
+	
+	@FXML
+    private Pane pane;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Battlefield battlefield = new Battlefield("battlefields/battlefield1.json");
+		int[][] battlefieldArray = battlefield.getBattlefield();
+		pane = new Pane();
+		try {
+			createView(battlefieldArray);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private BufferedImage cropImage(BufferedImage src, int number) {
+	      BufferedImage dest = src.getSubimage(number%160,number/10*16, 16,16);
+	      BufferedImage copyOfImage = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_INT_RGB);
+	      Graphics g = copyOfImage.createGraphics();
+	      g.drawImage(src, 0, 0, null);
+	      return dest; 
+	}
+	
+	//private WritableImage cropImage()
+	
+	void createView(int[][] array) throws IOException {
+		for ( int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[0].length ; j++) {
+				ImageView image = new ImageView();
+				Image src = SwingFXUtils.toFXImage(cropImage(ImageIO.read(new File("tileset/tileset.png")),array[i][j]), null );
+				image.setImage(src);
+				this.pane.getChildren().add(image);
+			}
+		}
+		System.out.println(this.pane.getChildren());
+	}
 }
