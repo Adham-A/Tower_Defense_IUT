52882a69ced2ce742f8e913f29d1c4f74d3c9093
adhamawad06@gmail.com <adhamawad06@gmail.com>
2020-05-11 11:31:02 +0200

    Added a BattlefieldLoader for Battlefield class

--- src/model/Battlefield.java
+++ src/model/Battlefield.java
@@ -1,68 +1,30 @@
+// This  class represents the battlefield with the IDs of the tiles used
 package model;
-
-
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.FileReader;
-import java.util.Iterator;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 public class Battlefield {
 	
 	private int width;
 	private int height;
 	private int[][] battlefield;
+	private BattlefieldLoader battlefieldLoader;
 	
 	public Battlefield(String path) {
-		parseTerrainFromFile(path);
+		this.battlefieldLoader = new BattlefieldLoader(path);
+		this.height = battlefieldLoader.parseBattlefieldHeight();
+		this.width = battlefieldLoader.parseBattlefieldWidth();
+		this.battlefield = battlefieldLoader.parseBattlefieldFromFile();
 	}
-	
-	public void parseTerrainFromFile(String path) {
-		Object obj = null;
-		try {
-			obj = new JSONParser().parse(new FileReader(path));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		JSONObject jo = (JSONObject) obj;
-		this.height = ((Long) jo.get("height")).intValue();
-		this.width = ((Long) jo.get("width")).intValue();
-		
-		this.battlefield = new int [this.width][this.height];
-
-		JSONArray jsonArray = (JSONArray) jo.get("data");
-		Iterator<?> iterator = jsonArray.iterator();
-
-		for (int line = 0; line < this.height; line++) {
-			for (int column = 0; column < this.width; column++) {
-				if(iterator.hasNext()) {
-					battlefield[column][line] = ( (Long) iterator.next()).intValue();
-				}
-			}
-		}
-		
-	}
-	
-	/*private BufferedImage cropImage(BufferedImage src, int number) {
-	      BufferedImage dest = src.getSubimage(0, 0, (number-((number%15)*15))*16,(number%15)*16);
-	      BufferedImage copyOfImage = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_INT_RGB);
-	      Graphics g = copyOfImage.createGraphics();
-	      g.drawImage(src, 0, 0, null);
-	      return dest; 
-	}*/
 	
 	public int getWidth() {
-		return width;
+		return this.width;
 	}
 
 	public int getHeight() {
-		return height;
+		return this.height;
 	}
 
-	public int[][] getBattlefield() {
-		return battlefield;
+	public int getBattlefieldTile(int x,int y) { 
+		return this.battlefield[x][y]; 
 	}
 
 }
