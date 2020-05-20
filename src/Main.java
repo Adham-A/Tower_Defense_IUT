59579aa2357f57fdcabbb561f3c915bebadd7b77
adhamawad06@gmail.com <adhamawad06@gmail.com>
2020-05-20 10:48:18 +0200

    Added view button

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.fxml.FXMLLoader;
 
@@ -9,7 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			TilePane root = FXMLLoader.load(getClass().getResource("/view/view.fxml"));
+			Pane root = FXMLLoader.load(getClass().getResource("/view/view.fxml"));
 
 			Scene scene = new Scene(root,480,864);
 			primaryStage.setScene(scene);
