ad4e8f145c938b2b0d95639b9c1cd84837fae6a1
stevexu4 <stevexu4@gmail.com>
2020-06-03 12:07:47 +0200

    Added remove cases in the method onChanged() in the class EnemyListListener.

--- src/controller/EnemyListListener.java
+++ src/controller/EnemyListListener.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.collections.ListChangeListener;
+import javafx.scene.image.ImageView;
 import model.enemy.Enemy;
 import view.BattlefieldView;
 
@@ -14,11 +15,17 @@ public class EnemyListListener implements ListChangeListener<Enemy>{
 	
 	@Override
 	public void onChanged(Change<? extends Enemy> c) {
-		while(c.next()) {
-		Enemy enemy = c.getAddedSubList().get(c.getAddedSize()-1);
-		battlefieldView.createEnemy(enemy);
+		while (c.next()) {
+			if (c.wasAdded()) {
+				Enemy enemy = c.getAddedSubList().get(c.getAddedSize() - 1);
+				battlefieldView.createEnemy(enemy);
+			}
+			if (c.wasRemoved()) {
+				Enemy enemy = c.getRemoved().get(c.getRemovedSize() - 1);
+				String id = enemy.getId();
+				ImageView imageView = (ImageView) this.battlefieldView.getPane().lookup("#" + id);
+				this.battlefieldView.getPane().getChildren().remove(imageView);
+			}
 		}
 	}
-	
-	
 }
