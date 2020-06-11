ce68337c296086095fe6032038bb4fc7148fa2c2
stevexu4 <stevexu4@gmail.com>
2020-06-11 18:31:51 +0200

    Added the class DwarfScientist and Potion.

---
+++ src/model/projectile/Potion.java
@@ -0,0 +1,26 @@
+package model.projectile;
+
+import model.enemy.Diamond;
+import model.enemy.Enemy;
+
+public class Potion extends Projectile {
+
+    public Potion(int damage, int speed, double x, double y) {
+        super(damage, speed, x, y);
+    }
+
+    public boolean shoot(Enemy e) {
+        boolean b = super.shoot(e);
+
+        if(e!=null && e instanceof Diamond && ((Diamond) e).getArmor()) {
+            debuff((Diamond) e);
+        }
+
+        return b;
+    }
+
+    public void debuff(Diamond d) {
+        d.setArmor(false);
+    }
+
+}
---
+++ src/model/turret/DwarfScientist.java
@@ -0,0 +1,12 @@
+package model.turret;
+
+import model.Battlefield;
+import model.projectile.Potion;
+
+public class DwarfScientist extends TargetedTurret {
+
+    public DwarfScientist(int x, int y, Battlefield battlefield, double range) {
+        super(17, x, y, battlefield, 1, range, new Potion(3, 5, x, y));
+    }
+
+}
