0c03603f5b1051bb6ecb3d273fb0717621ecfaab
adhamawad06@gmail.com <adhamawad06@gmail.com>
2020-06-11 16:19:52 +0200

    Added diamond

---
+++ src/model/enemy/Diamond.java
@@ -0,0 +1,33 @@
+package model.enemy;
+
+import model.Battlefield;
+import model.projectile.Projectile;
+
+public class Diamond extends Enemy{
+	
+	public boolean armor;
+	
+	public Diamond(int hp, int x, int y, Battlefield battlefield) {
+		super(hp, x, y, battlefield);
+		this.armor = true;
+	}
+	
+	@Override
+	public void removeHp(Projectile projectile) {
+		if(!armor) {
+			super.removeHp(projectile);
+		}
+	}
+	
+	@Override
+	public void removeHp(int damage) {
+		if(!armor) {
+			super.removeHp(damage);
+		}
+	}
+
+	public void setArmor(boolean b) {
+		this.armor = b;
+	}
+
+}
