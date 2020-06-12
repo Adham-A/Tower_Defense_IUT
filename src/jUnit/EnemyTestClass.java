571c5f6713d193edfdc70964fbce24fe274884f1
stevexu4 <stevexu4@gmail.com>
2020-06-12 13:26:02 +0200

    Updated the test class EnemyTestClass and Added the test class ProjectileTestClass.

--- src/jUnit/EnemyTestClass.java
+++ src/jUnit/EnemyTestClass.java
@@ -1,27 +1,39 @@
 package jUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import model.enemy.*;
+import model.turret.*;
+import model.projectile.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import exception.TerrainLoaderException;
 import model.Battlefield;
-import model.enemy.Diamond;
-import model.enemy.Emerald;
-import model.enemy.Quartz;
-import model.enemy.Saphir;
-import model.projectile.Pickaxe;
+
+import mineralsRevenge.mRUtil;
 
 
 public class EnemyTestClass {
-	
-	private Diamond diamond;
-	private Saphir saphir;
-	private Emerald emerald;
-	private Quartz quartz;
+
 	private Battlefield battlefield;
-	private Pickaxe damageDealer;
+
+	private Quartz quartz;
+	private Emerald emerald;
+	private Saphir saphir;
+	private Diamond diamond;
+
+	private Pickaxe pickaxe;
+	private Rocket rocket;
+	private Potion potion;
+	private Dynamite dynamite;
+
+	private DwarfMiner miner;
+	private DwarfSoldier soldier;
+	private DwarfDemolitionist demolitionist;
+	private DwarfScientist scientist;
 	
 	@BeforeEach
 	public void setUp() {
@@ -30,19 +42,78 @@ public class EnemyTestClass {
 		} catch (TerrainLoaderException e) {
 			e.printStackTrace();
 		}
-		this.damageDealer = new Pickaxe(20, 0, 15, 15);
-		this.diamond = new Diamond(80, 15, 15, battlefield);
-		this.saphir = new Saphir(20,15,15,battlefield);
-		this.emerald = new Emerald(15, 15, battlefield);
-		this.quartz = new Quartz(15, 15, battlefield);
-	}
-	
-	@Test
-	void diamondResistDamage() {
-		System.out.println(this.damageDealer);
-		System.out.println(this.diamond);
-		this.damageDealer.shoot(this.diamond);
-		assertEquals(this.diamond.getHp(), 80);
+
+		this.pickaxe = new Pickaxe(15, 15);
+		this.rocket = new Rocket(15, 15);
+		this.potion = new Potion(15, 15);
+		this.dynamite = new Dynamite(15, 15, battlefield);
+
+		this.miner = new DwarfMiner(3, 5, battlefield);
+		this.soldier = new DwarfSoldier(3, 6, battlefield);
+		this.demolitionist = new DwarfDemolitionist(3, 7, battlefield);
+		this.scientist = new DwarfScientist(4, 6, battlefield);
+
+		this.quartz = new Quartz(8, 22, battlefield);
+		this.emerald = new Emerald(7, 22,  battlefield);
+		this.saphir = new Saphir(5,7, battlefield);
+		this.diamond = new Diamond(7, 24,  battlefield);
+
+		this.battlefield.addEnemy(this.quartz);
+		this.battlefield.addEnemy(this.emerald);
+		this.battlefield.addEnemy(this.saphir);
+		this.battlefield.addEnemy(this.diamond);
 	}
 
+	@Test
+	void testDiamondResistsToDamage() {
+		this.pickaxe.shoot(this.diamond);
+		assertEquals(mRUtil.diamond_hp, this.diamond.getHp(), "Case: pickaxe shoot the diamond");
+		this.rocket.shoot(this.diamond);
+		assertEquals(mRUtil.diamond_hp, this.diamond.getHp(), "Case: rocket shoot the diamond");
+		this.dynamite.shoot(this.diamond);
+		assertEquals(mRUtil.diamond_hp, this.diamond.getHp(), "Case: dynamite shoot the diamond");
+		this.potion.shoot(this.diamond);
+		assertEquals(mRUtil.diamond_hp, this.diamond.getHp(), "Case: potion shoot the diamond");
+	}
+
+	@Test
+	void testSplitIntoTwo() {
+		for(Enemy e: this.battlefield.getEnemyList()) {
+			if(e instanceof Emerald) {
+				assertFalse(((Emerald) e).isAChild(), "Case: no emerald child before the split");
+			}
+		}
+
+		this.emerald.setHp(0);
+		this.emerald.action(); // it gives birth to 2 emeralds and removes the parent from the list.
+
+		for(Enemy e: this.battlefield.getEnemyList()) {
+			if(e instanceof Emerald) {
+				assertTrue(((Emerald) e).isAChild(), "Case: presence of emerald child after the split");
+			}
+		}
+	}
+
+	@Test
+	void testDealsBackDamages() {
+		this.miner.shoot();
+		assertEquals(mRUtil.saphir_hp-mRUtil.pickaxe_damage,this.saphir.getHp(), "");
+		assertEquals(mRUtil.dwarfMiner_hp-mRUtil.pickaxe_damage/8,this.miner.getHp(), "");
+
+		this.saphir.setHp(50);
+		this.soldier.shoot();
+		assertEquals(mRUtil.saphir_hp-mRUtil.rocket_damage,this.saphir.getHp(), "");
+		assertEquals(mRUtil.dwarfSoldier_hp-mRUtil.rocket_damage/8,this.soldier.getHp(), "");
+
+		this.saphir.setHp(50);
+		this.demolitionist.shoot();
+		assertEquals(mRUtil.saphir_hp-mRUtil.dynamite_damage,this.saphir.getHp(), "");
+		assertEquals(mRUtil.dwarfDemolitionist_hp-mRUtil.dynamite_damage/8,this.demolitionist.getHp(), "");
+
+		this.saphir.setHp(50);
+		this.scientist.shoot();
+		assertEquals(mRUtil.saphir_hp-mRUtil.potion_damage,this.saphir.getHp(), "");
+		assertEquals(mRUtil.dwarfScientist_hp-mRUtil.potion_damage/8,this.scientist.getHp(), "");
+
+	}
 }
---
+++ src/jUnit/ProjectileTestClass.java
@@ -0,0 +1,78 @@
+package jUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import model.enemy.*;
+import model.projectile.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import exception.TerrainLoaderException;
+import model.Battlefield;
+
+import mineralsRevenge.mRUtil;
+
+public class ProjectileTestClass {
+
+    private Battlefield battlefield;
+
+    private Quartz quartz;
+    private Emerald emerald;
+    private Saphir saphir;
+    private Diamond diamond;
+
+    private Rocket rocket;
+    private Potion potion;
+    private Dynamite dynamite;
+
+    @BeforeEach
+    public void setUp() {
+        try {
+            this.battlefield = new Battlefield("battlefields/battlefield1.json");
+        } catch (TerrainLoaderException e) {
+            e.printStackTrace();
+        }
+
+        this.rocket = new Rocket(15, 15);
+        this.potion = new Potion(15, 15);
+        this.dynamite = new Dynamite(15, 15, battlefield);
+
+        this.quartz = new Quartz(8, 20, battlefield);
+        this.emerald = new Emerald(8, 22,  battlefield);
+        this.saphir = new Saphir(7,22, battlefield);
+        this.diamond = new Diamond(7, 23,  battlefield);
+
+        this.battlefield.addEnemy(this.quartz);
+        this.battlefield.addEnemy(this.emerald);
+        this.battlefield.addEnemy(this.saphir);
+        this.battlefield.addEnemy(this.diamond);
+    }
+
+	@Test
+	void testPushBackEnemy() {
+		this.dynamite.shoot(this.quartz);
+		assertEquals(8, this.quartz.getX(), "Case: x");
+		assertEquals(22, this.quartz.getY(), "Case: y");
+	}
+
+	@Test
+	void testAreaOfEffects() {
+		this.rocket.shoot(this.emerald);
+
+		assertEquals(mRUtil.emerald_hp - mRUtil.rocket_damage, this.emerald.getHp(), "Case: target of the rocket");
+		assertEquals( mRUtil.quartz_hp - 3, this.quartz.getHp(), "Case: enemy who is one position ahead the target");
+		assertEquals(mRUtil.saphir_hp - 6, this.saphir.getHp(), "Case: enemy who is one position behind the target");
+		assertEquals(mRUtil.diamond_hp, this.diamond.getHp(), "Case: enemy out of the radius");
+	}
+
+    @Test
+    void testNullifyDiamondsArmor() {
+        this.potion.shoot(this.diamond);
+        assertFalse(this.diamond.hasArmor(), "Case: Potions Nullify Diamonds' Armor");
+
+        this.potion.shoot(this.diamond);
+        assertEquals(mRUtil.diamond_hp - mRUtil.potion_damage, this.diamond.getHp());
+    }
+}
